8d761086728cc81fd75a38b73237fa3c51bf388f
Stephen <stephengift43@gmail.com>
2023-08-27 09:38:24 +0100

    Update Main class

--- src/BankSystem/Main.java
+++ src/BankSystem/Main.java
@@ -16,10 +16,46 @@ public class Main {
 
 
         if ((atmNumber == atmnumber)&&(atmpin == atmPin)) {
-            System.out.println("Validation done");
-        }else {
+            while (true){
+                System.out.println("""
+                               Choose an option\s
+                                1. View Available balance\s
+                                2. Withdraw Amount\s
+                                3. Deposit Amount\s
+                                4. View Mini-Statement\s
+                                5. Exit\s
+                                Enter Choice: \s""");
+                System.out.println("Validation done");
+                int choice = scanner.nextInt();
+
+                 switch (choice){
+                    case 1:{
+
+                    }
+                    case 2:{
+
+                    }
+                    case 3:{
+
+                    }
+                    case 4:{
+
+                    }
+                    case 5:{
+                        System.out.println("Collect your ATM card \n" +
+                                "Thank you for using Stephen ATM Machine");
+                        System.exit(0);
+                    }
+                    default:{
+                        System.out.println("Please Enter correct choice: ");
+                    }
+            }
+            }
+    } else{
             System.out.println("Incorrect Number or Pin");
             System.exit(0);
         }
-    }
-}
+}}
+
+
+
