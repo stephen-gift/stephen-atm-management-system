ee21421dda415264efd56022c52beb6e9fec7017
Stephen <stephengift43@gmail.com>
2023-08-27 09:50:28 +0100

    update AtmOperationImplementation.java

--- src/BankSystem/AtmOperationImplementation.java
+++ src/BankSystem/AtmOperationImplementation.java
@@ -1,4 +1,24 @@
 package BankSystem;
 
-public class AtmOperationImplementation {
+public class AtmOperationImplementation implements AtmOperationInterface {
+
+    @Override
+    public void viewBalance() {
+
+    }
+
+    @Override
+    public void withdrawAmount(double withdrawAmount) {
+
+    }
+
+    @Override
+    public void depositAmount(double depositAmount) {
+
+    }
+
+    @Override
+    public void viewMiniStatement() {
+
+    }
 }
