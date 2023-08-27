aa343eef86fa526d793f43c531c8e3b1160d37b8
Stephen <stephengift43@gmail.com>
2023-08-27 07:57:55 +0100

    ATM - Basic operations

--- src/BankSystem/ATM.java
+++ src/BankSystem/ATM.java
@@ -1,2 +1,41 @@
-package BankSystem;public class ATM {
+package BankSystem;
+
+public class ATM {
+    private double balance;
+    private double depositAmount;
+    private double withdrawnAmount;
+
+    /* Default constructor */
+    ATM(){
+
+    }
+
+    /* Getters and setters */
+
+//    balance
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+//    depositAmount
+    public double getDepositAmount() {
+        return depositAmount;
+    }
+
+    public void setDepositAmount(double depositAmount) {
+        this.depositAmount = depositAmount;
+    }
+
+//    withdrawnAmount
+    public double getWithdrawnAmount() {
+        return withdrawnAmount;
+    }
+
+    public void setWithdrawnAmount(double withdrawnAmount) {
+        this.withdrawnAmount = withdrawnAmount;
+    }
 }
