9d92b9806b069c7eff85414e01436ac7e6642954
Stephen <stephengift43@gmail.com>
2023-08-27 09:45:30 +0100

    create AtmOperationInterface.java

--- src/BankSystem/AtmOperationInterface.java
+++ src/BankSystem/AtmOperationInterface.java
@@ -1,4 +1,8 @@
 package BankSystem;
 
-public class AtmOperationInterface {
+public interface AtmOperationInterface {
+    public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount);
+    public void viewMiniStatement();
 }
