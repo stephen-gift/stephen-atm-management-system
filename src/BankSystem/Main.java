a14f717ca3bf2332e7baf250b6136d1c624a67ea
Stephen <stephengift43@gmail.com>
2023-08-27 08:18:21 +0100

    Main class

--- src/BankSystem/Main.java
+++ src/BankSystem/Main.java
@@ -1,4 +1,25 @@
 package BankSystem;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        int atmnumber = 12345;
+        int atmpin = 123;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Stephen ATM Machine !!!");
+        System.out.println("Enter your Atm Number: ");
+        int atmNumber = scanner.nextInt();
+
+        System.out.println("Enter your PIN: ");
+        int atmPin = scanner.nextInt();
+
+
+        if ((atmNumber == atmnumber)&&(atmpin == atmPin)) {
+            System.out.println("Validation done");
+        }else {
+            System.out.println("Incorrect Number or Pin");
+            System.exit(0);
+        }
+    }
 }
